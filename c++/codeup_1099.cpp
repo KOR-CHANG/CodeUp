c351eac5e1b1160a4f2f82865bbe511ece2cee73
Leechangryeol <dlckdfuf141@naver.com>
2021-02-13 23:41:20 +0900

    1099

---
+++ c++/codeup_1099.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+	int x,y;
+    int a[11][11] = {};
+    for(int i =1; i<=10; i++)
+    	for(int j = 1; j<=10; j++)
+    		scanf("%d", &a[i][j]);
+    		
+    x = 2;
+    y = 2;
+    while(1)
+    {
+    	if(a[x][y] == 0)
+    	{
+    		a[x][y] = 9;
+    		y++;
+		}
+		if(a[x][y] == 1)
+			y--, x++;
+		if(a[x][y] == 2)
+		{
+			a[x][y] = 9;
+			break;
+		}else if(a[x][y+1] == 1&& a[x+1][y] == 1){
+			if(a[x][y] == 0)
+			{
+				a[x][y] = 9;
+			}
+			break;
+		}
+	}
+    
+    for(int i =1; i<=10; i++)
+    {
+		for(int j = 1; j<=10; j++)
+    	{
+    		printf("%d ", a[i][j]);	
+		}	
+		printf("\n");
+	}
+    return 0;
+}
