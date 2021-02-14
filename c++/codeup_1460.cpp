fa0543de091ae7e02adef32c43588c713b742f0b
Leechangryeol <dlckdfuf141@naver.com>
2021-02-14 13:41:02 +0900

    1460

---
+++ c++/codeup_1460.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, k = 1;
+	scanf("%d", &n);
+	int a[n+1][n+1];
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= n; j++)
+		{
+			a[i][j]= k++;
+		}
+	}
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= n; j++)
+		{
+			printf("%d ", a[i][j]);	
+		}
+		printf("\n");
+	}
+	return 0;
+}
